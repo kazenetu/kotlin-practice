ee2064c7426887d1c6c70a989bb835583a870463
kazenetu <rakudafactory@gmail.com>
2020-02-09 23:11:49 +0900

    文字列の長さ取得メソッドを追加

--- basic/nullable.kt
+++ basic/nullable.kt
@@ -4,11 +4,11 @@
 fun main() {
     // 文字列のnull許容
     var stringLocalField:String? = null
-    println("stringLocalField:[$stringLocalField]")
+    println("stringLocalField:[$stringLocalField] length="+length(stringLocalField))
 
     // 文字列の変更
     stringLocalField="chenged stringLocalField"
-    println("update stringLocalField:[$stringLocalField]")
+    println("update stringLocalField:[$stringLocalField] length="+length(stringLocalField))
 
     // 数値のnull許容
     var intLocalField:Int? = null
@@ -19,8 +19,19 @@ fun main() {
     println("update intLocalField:[$intLocalField]")
 }
 
+/*
+ * 文字列の長さを取得
+ */
+fun length(src: String?): Int {
+    if(src == null){
+        return 0
+    }else{
+        return src.count()
+    }
+}
+
 // 出力：
-// stringLocalField:[null]
-// update stringLocalField:[chenged stringLocalField]
+// stringLocalField:[null] length=0
+// update stringLocalField:[chenged stringLocalField] length=24
 // intLocalField:[null]
 // update intLocalField:[100]
