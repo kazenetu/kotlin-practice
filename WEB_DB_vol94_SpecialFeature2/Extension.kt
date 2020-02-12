be8a108534f04a4672dc0e8bd0daef86df024700
kazenetu <rakudafactory@gmail.com>
2020-02-12 20:17:19 +0900

    null許容タイプを追加

--- WEB_DB_vol94_SpecialFeature2/Extension.kt
+++ WEB_DB_vol94_SpecialFeature2/Extension.kt
@@ -4,10 +4,28 @@
 fun String.toUpperSpaceLess() = this.toUpperCase().replace(" ", "")
 
 /*
+ * 拡張関数:大文字+スペース除去(null許容)
+ */
+fun String?.toUpperSpaceLessNullable() = if(this == null) "null!!" else this?.toUpperCase().replace(" ", "")
+
+/*
  * メソッド実行
  */
 fun main() {
+    // 大文字+スペース除去
+    println("----String.toUpperSpaceLess----")
     println("Hello, world!!!".toUpperSpaceLess())
+
+    // 大文字+スペース除去(null許容)
+    println("----String.toUpperSpaceLess----")
+    println("Hello, world!!!".toUpperSpaceLessNullable())
+    val nullString: String? = null;
+    println(nullString.toUpperSpaceLessNullable())
 }
 
-// 出力：HELLO,WORLD!!!
+// 出力：
+// ----String.toUpperSpaceLess----
+// HELLO,WORLD!!!
+// ----String.toUpperSpaceLess----
+// HELLO,WORLD!!!
+// null!!
