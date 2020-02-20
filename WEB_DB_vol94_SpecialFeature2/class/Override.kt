b5bd67beaf2824f6250513510b85f63c54e3772c
kazenetu <rakudafactory@gmail.com>
2020-02-20 22:05:04 +0900

    クラス継承を追加

---
+++ WEB_DB_vol94_SpecialFeature2/class/Override.kt
@@ -0,0 +1,61 @@
+/*
+ * スーパークラス
+ */
+open class SuperClass(val age: Int) {
+    open fun print() {
+        println("SuperClass age=$age")
+    }
+}
+
+/*
+ * サブクラス:プライマリコンストラクタで継承
+ * 　プライマリコンストラクタにパラメータ付きのスーパークラスを宣言
+ */
+class SubClassPrimary(age: Int): SuperClass(age) {
+    override fun print() {
+        println("SubClass age=$age")
+    }
+}
+
+/*
+ * サブクラス:セカンダリコンストラクタで継承
+ * 　プライマリコンストラクタにパラメータなしのスーパークラスを宣言
+ * 　セカンダリコンストラクタにパラメータ付きでsuperでスーパークラスのコンストラクタを宣言
+ */
+class SubClassSecondary: SuperClass {
+    constructor(age: Int): super(age)
+
+    override fun print() {
+        println("SubClass age=$age")
+    }
+}
+
+
+/*
+ * エントリポイント
+ */
+fun main() {
+
+    // スーパークラス
+    println("----SuperClass---")
+    val superClass = SuperClass(10)
+    superClass.print()
+
+    // サブクラス：プライマリコンストラクタの呼び出し
+    println("----SubClass PrimaryConstructor---")
+    val subClassPrimary = SubClassPrimary(20)
+    subClassPrimary.print()
+
+    // サブクラス：セカンダリコンストラクタの呼び出し
+    println("----SubClass SecondaryConstructor---")
+    val subClassSecondary = SubClassSecondary(30)
+    subClassSecondary.print()
+}
+
+// 出力：
+// ----SuperClass---
+// SuperClass age=10
+// ----SubClass PrimaryConstructor---
+// SubClass age=20
+// ----SubClass SecondaryConstructor---
+// SubClass age=30
