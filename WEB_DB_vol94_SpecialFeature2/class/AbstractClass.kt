11e2b450f80ee9e5da28cf0929362c5a9526e0ab
kazenetu <rakudafactory@gmail.com>
2020-02-21 21:54:04 +0900

    抽象クラスを追加

---
+++ WEB_DB_vol94_SpecialFeature2/class/AbstractClass.kt
@@ -0,0 +1,68 @@
+/*
+ * 抽象クラス
+ */
+abstract class AbstractClass(val age: Int) {
+    /*
+     * 抽象メソッド：protectedスコープ
+     */
+    abstract protected fun printMain()
+
+    /*
+     * エントリメソッド
+     */
+    fun print() {
+        printMain()
+    }
+}
+
+/*
+ * サブクラスA
+ */
+class SubClassA(age: Int): AbstractClass(age) {
+    /*
+     * 具象メソッド：protectedスコープ
+     */
+    override fun printMain() {
+        println("SubClassA age=$age")
+    }
+}
+
+/*
+ * サブクラスB
+ */
+class SubClassB(age: Int): AbstractClass(age) {
+    /*
+     * 具象メソッド：protectedスコープ
+     */
+    override fun printMain() {
+        println("SubClassB age=$age")
+    }
+}
+
+/*
+ * エントリポイント
+ */
+fun main() {
+
+    // 抽象クラスは生成できない
+    println("----AbstractClass---")
+    //val abstractClass = AbstractClass(10)
+    //abstractClass.print()
+
+    // サブクラスAの呼び出し
+    println("----SubClassA---")
+    val subClassA = SubClassA(20)
+    subClassA.print()
+
+    // サブクラスBの呼び出し
+    println("----SubClassB---")
+    val subClassB = SubClassB(20)
+    subClassB.print()
+}
+
+// 出力：
+// ----AbstractClass---
+// ----SubClassA---
+// SubClassA age=20
+// ----SubClassB---
+// SubClassB age=20
