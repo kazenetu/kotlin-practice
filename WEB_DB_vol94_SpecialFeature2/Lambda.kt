3f12cac31ee19262c5f4a3e8fad2dcecb32f30e6
kazenetu <rakudafactory@gmail.com>
2020-03-05 21:16:09 +0900

    戻り値を含めるラムダ式の実装例を追加

--- WEB_DB_vol94_SpecialFeature2/Lambda.kt
+++ WEB_DB_vol94_SpecialFeature2/Lambda.kt
@@ -1,25 +1,41 @@
 /*
- * ラムダ式
+ * ラムダ式：メソッド呼び出し
  */
 fun run(function:()->Unit){
     function()
 }
 
+//-------------------------
+/*
+ * ラムダ式：メソッド呼び出し：戻り値
+ */
+fun calc(value: Int, op:(Int)->Int): Int{
+    return op(value)
+}
 
+//-------------------------
 /*
  * エントリポイント
  */
 fun main() {
 
-    /*
-     * ラムダ式の利用
-     */
+    // ラムダ式の利用
+    println("----run---")
     run({
         println("ABC")
         println("123")
     })
+
+    // ラムダ式の利用
+    println("----calc---")
+    println("10+5=${ calc(10, {x -> x + 5}) }")
+    println("10*5=${ calc(10, {it * 5}) }")
 }
 
 // 出力：
+// ----run---
 // ABC
 // 123
+// ----calc---
+// 10+5=15
+// 10*5=50
