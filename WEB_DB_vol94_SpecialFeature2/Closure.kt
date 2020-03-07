57c4176aa554a9909e6fb776f063a20580938c82
kazenetu <rakudafactory@gmail.com>
2020-03-07 22:06:35 +0900

    クロージャの実装例を追加

---
+++ WEB_DB_vol94_SpecialFeature2/Closure.kt
@@ -0,0 +1,55 @@
+/*
+ * クロージャー例：メソッド内ローカルフィールドのインクリメント
+ */
+fun incrementClosure(): ()->Int {
+    var count:Int = 0
+    return fun(): Int {
+        return ++count
+    }
+}
+
+//-------------------------
+/*
+ * クロージャー例：パラメータの値の参照
+ */
+fun addClosure(a:Int): (Int)->Int {
+    return fun(b:Int) :Int {
+        return a + b
+    }
+}
+
+//-------------------------
+/*
+ * エントリポイント
+ */
+fun main() {
+    // メソッド内ローカルフィールドのインクリメント
+    println("----inner var increment closure----")
+    val iClosure = incrementClosure()
+    println(iClosure())
+    println(iClosure())
+    println(iClosure())
+
+    // パラメータの値の参照
+    println("----return function closure----")
+    val aClosure = addClosure(10)
+    println(aClosure(5))
+
+    // ラムダ：範囲対象外のローカルフィールドの更新
+    println("----lamda closure----")
+    var sum: Int = 0;
+    listOf(1, 2, 3, 4, 5, 6).forEach {
+        sum += it
+    }
+    println(sum)
+}
+
+// 出力：
+// ----inner var increment closure----
+// 1
+// 2
+// 3
+// ----return function closure----
+// 15
+// ----lamda closure----
+// 21
