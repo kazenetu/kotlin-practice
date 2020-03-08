0ec81d57b9ba1a01e000cc1401bba6ccdb753a04
kazenetu <rakudafactory@gmail.com>
2020-03-08 22:08:56 +0900

    高階関数の実装例を追加

---
+++ WEB_DB_vol94_SpecialFeature2/HigherOrderFunctions.kt
@@ -0,0 +1,38 @@
+/*
+ * 高階関数
+ * エントリポイント
+ */
+fun main() {
+    // filter
+    println("----filter----")
+    println(listOf(1, 2, 3, 4, 5).filter{it % 2 == 0})
+    val filterSourceData = listOf(1, "2", 3, 4.5, 5.0f, 6)
+    println(filterSourceData.filter{it is String})
+    println(filterSourceData.filter{it is Double})
+    println(filterSourceData.filter{it is Int && it % 2 == 0})
+
+    // map
+    println("----map----")
+    val mapSourceData = listOf("a", "b", "Hello, world!!!")
+    println(mapSourceData.map{it.toUpperCase()})
+
+    // forEach(戻り値はUnit)
+    println("----forEach----")
+    val forEachSourceData = listOf("a", "b", "Hello, world!!!")
+    forEachSourceData.forEach{println(it)}
+
+    // reduce
+    println("----reduce----")
+    println(listOf(1, 2, 3, 4, 5).reduce{a ,b -> a + b})
+
+}
+
+// 出力：
+// ----inner var increment closure----
+// 1
+// 2
+// 3
+// ----return function closure----
+// 15
+// ----lamda closure----
+// 21
