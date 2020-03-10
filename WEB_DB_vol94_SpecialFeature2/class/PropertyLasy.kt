5a9e18c823bea8bc0d481790daf7d94c47c62826
kazenetu <rakudafactory@gmail.com>
2020-03-10 22:06:12 +0900

    lazyプロパティの実装例を追加

---
+++ WEB_DB_vol94_SpecialFeature2/class/PropertyLasy.kt
@@ -0,0 +1,33 @@
+/*
+ * デリゲートプロパティ実装例
+ */
+class LasyPropertyClass() {
+    /*
+     * lazyデリゲート
+     * ・どんな型も宣言可能。
+     * ・valで宣言。
+     * ・値の変更不可能。
+     * ・初めて使用する際に初期化するため、エラーが発生しない。
+     */
+    val age: Int by lazy {
+        print("computed! : ")
+        10
+    }
+}
+
+/*
+ * エントリポイント
+ */
+fun main() {
+
+    // デリゲートプロパティテスト
+    println("----LasyPropertyClass---")
+    val prop = LasyPropertyClass()
+    println(prop.age)
+    println(prop.age)
+}
+
+// 出力：
+// ----LasyPropertyClass---
+// computed! : 10
+// 10
