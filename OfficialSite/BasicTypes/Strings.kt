cd50840435ad5222c65d3dade6694b7f99a56aee
kazenetu <rakudafactory@gmail.com>
2020-03-17 21:47:51 +0900

    文字列型の実装例を追加

---
+++ OfficialSite/BasicTypes/Strings.kt
@@ -0,0 +1,70 @@
+/*
+ * 基本型：文字列型
+ */
+fun main() {
+
+    // 文字列型
+    println("---Strings---")
+    val s = "Hello, world!"
+    println("single line = $s")
+
+    val textln = "Hello,\nworld!"
+    println("multi line ln =")
+    println(textln)
+
+    val multiLine = """
+    Line1
+    Line2
+      Line3
+    """
+    println("multi line =")
+    println(multiLine)
+
+    val multiLineTrim = """
+    |Line1
+    |Line2
+    |  Line3
+    """.trimMargin()
+    println("multi line trimMargin('|') =")
+    println(multiLineTrim)
+
+    val multiLineTrimlt = """
+        <Line1
+        <Line2
+        <  Line3
+    """.trimMargin("<")
+    println("multi line trimMargin('<') =")
+    println(multiLineTrimlt)
+
+    println("---access character---")
+    val accesscharacter = "abc"
+    for (c in accesscharacter) {
+        println(c)
+    }
+
+}
+
+// 出力：
+// ---Strings---
+// single line = Hello, world!
+// multi line ln =
+// Hello,
+// world!
+// multi line =
+//
+//     Line1
+//     Line2
+//       Line3
+//
+// multi line trimMargin('|') =
+// Line1
+// Line2
+//   Line3
+// multi line trimMargin('<') =
+// Line1
+// Line2
+//   Line3
+// ---access character---
+// a
+// b
+// c
