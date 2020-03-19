056699e374de903bfb7d73b466f9725e5afe5416
kazenetu <rakudafactory@gmail.com>
2020-03-20 08:33:55 +0900

    メソッドの別名の実装例を追加

--- OfficialSite/TypeAliases.kt
+++ OfficialSite/TypeAliases.kt
@@ -2,6 +2,10 @@
 typealias Strings = MutableList<String>
 typealias StringKeyMap<V> = MutableMap<String, V>
 
+// メソッドの別名設定
+typealias Callback = (text:String) -> Unit
+typealias Equals = (string1:String, string2:String) -> Boolean
+
 /*
  * 基本型：別名設定
  */
@@ -17,9 +21,20 @@ fun main() {
     map.put("A", 100)
     map.put("B", 1000)
     println("StringKeyMap<Int> = $map")
+
+    // メソッドの別名
+    println("---Typealiases Method---")
+    val callback:Callback = { println("Callback $it") }
+    callback("ABC")
+    val equals:Equals = { string1,string2 -> string1 == string2 }
+    println("equals(\"a\",\"a\") = ${equals("a","a")}")
+
 }
 
 // 出力：
 // ---Typealiases Collection---
 // stringList = [A, B, C]
 // StringKeyMap<Int> = {A=100, B=1000}
+// ---Typealiases Method---
+// Callback ABC
+// equals("a","a") = true
