32719486e6618530667c2ec2154609fb048653cc
kazenetu <rakudafactory@gmail.com>
2020-03-25 21:28:54 +0900

    変換の実装例を追加

--- OfficialSite/Collections/List.kt
+++ OfficialSite/Collections/List.kt
@@ -4,6 +4,11 @@
 data class UserInfo(val name: String, val age: Int)
 
 /*
+ * 変換：平坦化用データクラス
+ */
+data class StringContainer(val values: List<String>)
+
+/*
  * コレクション：ReadOnlyList
  */
 fun main() {
@@ -27,6 +32,28 @@ fun main() {
     println("> isEmpty") // リストの空チェック
     println("emptyList<String>().isEmpty() = ${ emptyList<String>().isEmpty() }")
     println("listOf(1, 2, 3, 4).isEmpty() = ${listOf(1, 2, 3, 4).isEmpty()}")
+
+    println("> Transformations:Flattening") // 変換：平坦化
+    println("listOf(listOf(1, 2, 3, 4), listOf(10, 20, 30, 40)).flatten() = ${ listOf(listOf(1, 2, 3, 4), listOf(10, 20, 30, 40)).flatten() }")
+    val stringContainers = listOf(
+        StringContainer(listOf("one", "two", "three")),
+        StringContainer(listOf("four", "five", "six")),
+        StringContainer(listOf("seven", "eight"))
+    )
+    println("stringContainers = $stringContainers")
+    println("stringContainers.flatMap { it.values } = ${stringContainers.flatMap { it.values }}") // データクラスでの平坦化
+
+    println("> Transformations:String representation") // 変換：コレクションの文字列化
+    println("listOf(\"one\", \"two\", \"three\").joinToString() = ${ listOf("one", "two", "three").joinToString() }")
+    println("listOf(\"one\", \"two\", \"three\").joinToString(separator = \" | \", prefix = \"start: \", postfix = \": end\") = ${ listOf("one", "two", "three").joinToString(separator = " | ", prefix = "start: ", postfix = ": end") }")
+    println("listOf(\"one\", \"two\", \"three\", \"for\").joinToString(limit = 3, truncated =\"<next...>\") = ${ listOf("one", "two", "three", "for").joinToString(limit = 3, truncated = "<next...>") }")
+    println("listOf(\"one\", \"two\", \"three\", \"for\").joinToString(limit = 3, truncated =\"<next...>\", separator = \" \") = ${ listOf("one", "two", "three", "for").joinToString(limit = 3, truncated = "<next...>", separator = " ") }")
+    println("listOf(\"one\", \"two\", \"three\", \"for\").joinToString { \"<\${it.toUpperCase()}>\" } = ${ listOf("one", "two", "three", "for").joinToString { "<${it.toUpperCase()}>" } }")
+    println("listOf(\"one\", \"two\", \"three\", \"for\").joinToString(transform = { \"<\${it.toUpperCase()}>\" }) = ${ listOf("one", "two", "three", "for").joinToString(transform = { "<${it.toUpperCase()}>" }) }")
+
+    val stringBuffer = StringBuffer("The list of numbers: ") // バッファにコピー
+    listOf("one", "two", "three").joinTo(stringBuffer)
+    println("listOf(\"one\", \"two\", \"three\").joinTo(stringBuffer) = $stringBuffer")
 
     // 各要素へのアクセス
     println("---ReadOnly List:Access element---")
@@ -86,6 +113,18 @@ fun main() {
 // > isEmpty
 // emptyList<String>().isEmpty() = true
 // listOf(1, 2, 3, 4).isEmpty() = false
+// > Transformations:Flattening
+// listOf(listOf(1, 2, 3, 4), listOf(10, 20, 30, 40)).flatten() = [1, 2, 3, 4, 10, 20, 30, 40]
+// stringContainers = [StringContainer(values=[one, two, three]), StringContainer(values=[four, five, six]), StringContainer(values=[seven, eight])]
+// stringContainers.flatMap { it.values } = [one, two, three, four, five, six, seven, eight]
+// > Transformations:String representation
+// listOf("one", "two", "three").joinToString() = one, two, three
+// listOf("one", "two", "three").joinToString(separator = " | ", prefix = "start: ", postfix = ": end") = start: one | two | three: end
+// listOf("one", "two", "three", "for").joinToString(limit = 3, truncated ="<next...>") = one, two, three, <next...>
+// listOf("one", "two", "three", "for").joinToString(limit = 3, truncated ="<next...>", separator = " ") = one two three <next...>
+// listOf("one", "two", "three", "for").joinToString { "<${it.toUpperCase()}>" } = <ONE>, <TWO>, <THREE>, <FOR>
+// listOf("one", "two", "three", "for").joinToString(transform = { "<${it.toUpperCase()}>" }) = <ONE>, <TWO>, <THREE>, <FOR>    
+// listOf("one", "two", "three").joinTo(stringBuffer) = The list of numbers: one, two, three
 // ---ReadOnly List:Access element---
 // list = [1, 2, 3, 4]
 // > get element
