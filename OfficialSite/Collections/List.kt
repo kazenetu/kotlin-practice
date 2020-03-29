6ebcb9e0229cb7fedc0c9eed92320718ce3f74ef
kazenetu <rakudafactory@gmail.com>
2020-03-29 22:28:05 +0900

    「単一要素の取得」の実装例を追加

--- OfficialSite/Collections/List.kt
+++ OfficialSite/Collections/List.kt
@@ -130,6 +130,39 @@ fun main() {
     println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").zipWithNext() = ${ listOf("one", "two", "three", "four", "five", "six").zipWithNext() }") 
     println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").zipWithNext { s1, s2 -> s1.length > s2.length } = ${ listOf("one", "two", "three", "four", "five", "six").zipWithNext { s1, s2 -> s1.length > s2.length } }") 
 
+    println("> Retrieving Single Elements:Retrieving by position") // 単一の要素の取得
+    println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").elementAt(3) = ${ listOf("one", "two", "three", "four", "five", "six").elementAt(3) }") 
+    println("sortedSetOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").elementAt(0) = ${ sortedSetOf("one", "two", "three", "four", "five", "six").elementAt(0) }") 
+
+    println("> Retrieving Single Elements:Retrieving by position:first and the last element") // 単一の要素の取得:先頭/末端の要素の取得
+    println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").first() = ${ listOf("one", "two", "three", "four", "five", "six").first() }") 
+    println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").last() = ${ listOf("one", "two", "three", "four", "five", "six").last() }") 
+
+    println("> Retrieving Single Elements:Retrieving by position:non-existing positions") // 単一の要素の取得:存在しないケース
+    println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").elementAtOrNull(6) = ${ listOf("one", "two", "three", "four", "five", "six").elementAtOrNull(6) }") 
+    println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").elementAtOrElse(6) { index -> \"The value for index \$index is undefined\"} = ${ listOf("one", "two", "three", "four", "five", "six").elementAtOrElse(6) { index -> "The value for index $index is undefined" } }") 
+
+    println("> Retrieving Single Elements:Retrieving by condition:first and the last element") // 単一の要素の取得:条件付き:先頭/末端の要素の取得
+    println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").first { it.length > 3 }  = ${ listOf("one", "two", "three", "four", "five", "six").first { it.length > 3 } }") 
+    println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").last { it.startsWith(\"f\") } = ${ listOf("one", "two", "three", "four", "five", "six").last { it.startsWith("f") } }") 
+
+    println("> Retrieving Single Elements:Retrieving by condition:first and the last element:non-existing:use aliases") // 単一の要素の取得:条件付き:先頭/末端の要素の取得:存在しないケース：使用推奨のエイリアス
+    println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").find { it.length > 5 }  = ${ listOf("one", "two", "three", "four", "five", "six").find { it.length > 5 } }") 
+    println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").findLast { it.startsWith(\"V\") } = ${ listOf("one", "two", "three", "four", "five", "six").findLast { it.startsWith("V") } }") 
+
+    println("> Retrieving Single Elements:Retrieving by condition:first and the last element:non-existing") // 単一の要素の取得:条件付き:先頭/末端の要素の取得:存在しないケース:非推奨
+    println("[use find]    listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").firstOrNull { it.length > 5 }  = ${ listOf("one", "two", "three", "four", "five", "six").firstOrNull { it.length > 5 }  }") 
+    println("[use findLst] listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").lastOrNull { it.startsWith(\"V\") } = ${ listOf("one", "two", "three", "four", "five", "six").lastOrNull { it.startsWith("V") } }") 
+
+    println("> Retrieving Single Elements:Checking existence") // 単一の要素の取得:存在確認
+    println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").contains(\"four\")  = ${ listOf("one", "two", "three", "four", "five", "six").contains("four") }") 
+    println("\"one\" in listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\")  = ${ "one" in listOf("one", "two", "three", "four", "five", "six") }") 
+    
+    println("> Retrieving Single Elements:Checking existence:multiple") // 単一の要素の取得:存在確認:複数一致
+    println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").containsAll(listOf(\"one\", \"four\"))  = ${ listOf("one", "two", "three", "four", "five", "six").containsAll(listOf("one", "four")) }") 
+    println("listOf(\"one\", \"two\", \"three\", \"four\", \"five\", \"six\").containsAll(listOf(\"zero\", \"four\"))  = ${ listOf("one", "two", "three", "four", "five", "six").containsAll(listOf("zero", "four")) }") 
+
+
     // 各要素へのアクセス
     println("---ReadOnly List:Access element---")
     val list = listOf(1, 2, 3, 4)
@@ -257,6 +290,30 @@ fun main() {
 // (0..4).toList().zipWithNext() = [(0, 1), (1, 2), (2, 3), (3, 4)]
 // listOf("one", "two", "three", "four", "five", "six").zipWithNext() = [(one, two), (two, three), (three, four), (four, five), (five, six)]
 // listOf("one", "two", "three", "four", "five", "six").zipWithNext { s1, s2 -> s1.length > s2.length } = [false, false, true, false, true]
+// > Retrieving Single Elements:Retrieving by position
+// listOf("one", "two", "three", "four", "five", "six").elementAt(3) = four
+// sortedSetOf("one", "two", "three", "four", "five", "six").elementAt(0) = five
+// > Retrieving Single Elements:Retrieving by position:first and the last element
+// listOf("one", "two", "three", "four", "five", "six").first() = one
+// listOf("one", "two", "three", "four", "five", "six").last() = six
+// > Retrieving Single Elements:Retrieving by position:non-existing positions
+// listOf("one", "two", "three", "four", "five", "six").elementAtOrNull(6) = null
+// listOf("one", "two", "three", "four", "five", "six").elementAtOrElse(6) { index -> "The value for index $index is undefined"} = The value for index 6 is undefined
+// > Retrieving Single Elements:Retrieving by condition:first and the last element
+// listOf("one", "two", "three", "four", "five", "six").first { it.length > 3 }  = three
+// listOf("one", "two", "three", "four", "five", "six").last { it.startsWith("f") } = five
+// > Retrieving Single Elements:Retrieving by condition:first and the last element:non-existing:use aliases
+// listOf("one", "two", "three", "four", "five", "six").find { it.length > 5 }  = null
+// listOf("one", "two", "three", "four", "five", "six").findLast { it.startsWith("V") } = null
+// > Retrieving Single Elements:Retrieving by condition:first and the last element:non-existing
+// [use find]    listOf("one", "two", "three", "four", "five", "six").firstOrNull { it.length > 5 }  = null
+// [use findLst] listOf("one", "two", "three", "four", "five", "six").lastOrNull { it.startsWith("V") } = null
+// > Retrieving Single Elements:Checking existence
+// listOf("one", "two", "three", "four", "five", "six").contains("four")  = true
+// "one" in listOf("one", "two", "three", "four", "five", "six")  = true
+// > Retrieving Single Elements:Checking existence:multiple
+// listOf("one", "two", "three", "four", "five", "six").containsAll(listOf("one", "four"))  = true
+// listOf("one", "two", "three", "four", "five", "six").containsAll(listOf("zero", "four"))  = false
 // ---ReadOnly List:Access element---
 // list = [1, 2, 3, 4]
 // > get element
