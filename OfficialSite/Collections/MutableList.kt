63d3b389e5dc7254bad366112e3ae4b17759bb35
kazenetu <rakudafactory@gmail.com>
2020-04-02 22:11:22 +0900

    並び替えの実装例を追加

--- OfficialSite/Collections/MutableList.kt
+++ OfficialSite/Collections/MutableList.kt
@@ -302,6 +302,40 @@ fun main() {
     println("binaryList.binarySearch(11) = ${ binaryList.binarySearch(11) }") // 要素インデックスを取得する
     println("　")
 
+    // 並び替え
+    println("---Mutable List : Collection Ordering---")
+
+    println("> Natural order")
+    println("mutableListOf(\"one\", \"two\", \"three\", \"four\").sorted()  = ${ mutableListOf("one", "two", "three", "four").sorted() }") 
+    println("mutableListOf(\"one\", \"two\", \"three\", \"four\").sortedDescending()  = ${ mutableListOf("one", "two", "three", "four").sortedDescending() }") 
+    println("　")
+
+    println("> Custom orders")
+    println("mutableListOf(\"one\", \"two\", \"three\", \"four\").sortedBy { it.length }  = ${ mutableListOf("one", "two", "three", "four").sortedBy { it.length } }") 
+    println("mutableListOf(\"one\", \"two\", \"three\", \"four\").sortedByDescending { it.length }  = ${ mutableListOf("one", "two", "three", "four").sortedByDescending { it.length } }") 
+    println("　")
+
+    println("> Custom orders:custom order for the collection sorting")
+    println("mutableListOf(\"one\", \"two\", \"three\", \"four\").sortedWith(compareBy { it.length })  = ${ mutableListOf("one", "two", "three", "four").sortedWith(compareBy { it.length }) }") 
+    println("　")
+
+    println("> Reverse order")
+    println("mutableListOf(\"one\", \"two\", \"three\", \"four\").reversed()  = ${ mutableListOf("one", "two", "three", "four").reversed() }") 
+    println("　")
+
+    println("> Reverse order:all its changes reflect in its reversed") // 参照元のリストを反映した逆順ソート
+    val numbers = mutableListOf("one", "two", "three", "four")
+    val reversedNumbers = numbers.asReversed()
+    println("reversedNumbers = mutableListOf(\"one\", \"two\", \"three\", \"four\").asReversed() : ${ reversedNumbers }") 
+    numbers.add("five")
+    println("numbers.add(\"five\")") 
+    println("reversedNumbers = ${ reversedNumbers }") 
+    println("　")
+
+    println("> Random order")
+    println("mutableListOf(\"one\", \"two\", \"three\", \"four\").shuffled()  = ${ mutableListOf("one", "two", "three", "four").shuffled() }") 
+    println("　")
+
 }
 
 // 出力：
@@ -468,8 +502,8 @@ fun main() {
 // writeList.sortBy(){ it % 10 } = [-1, 12, 2, 13, 3, 4, 15]
 // writeList.sortByDescending(){ it % 10 } = [15, 4, 13, 3, 12, 2, -1]
 // 　
-// writeList.shuffle() = [2, -1, 15, 13, 12, 4, 3]
-// writeList.reverse() = [3, 4, 12, 13, 15, -1, 2]
+// writeList.shuffle() = [3, 2, 13, 15, 4, 12, -1]
+// writeList.reverse() = [-1, 12, 4, 15, 13, 2, 3]
 // 　
 // ---Mutable List : Access element---
 // list = [1, 2, 3, 4]
@@ -503,6 +537,29 @@ fun main() {
 // users.binarySearch(UserInfo("C",100), compareBy<UserInfo> { it.name }.thenBy{ it.age }) = 2
 // 　
 // ---Mutable List : Binary search in sorted lists---
-// binaryList.shuffle() = [20, 8, 4, 9, 19, 2, 13, 7, 11, 12, 14, 18, 16, 5, 3, 6, 17, 10, 1, 15]
+// binaryList.shuffle() = [8, 19, 5, 1, 16, 7, 20, 4, 10, 14, 9, 11, 12, 2, 17, 13, 18, 3, 6, 15]
 // binaryList.sort() = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 // binaryList.binarySearch(11) = 10
+// 　
+// ---Mutable List : Collection Ordering---
+// > Natural order
+// mutableListOf("one", "two", "three", "four").sorted()  = [four, one, three, two]
+// mutableListOf("one", "two", "three", "four").sortedDescending()  = [two, three, one, four]
+// 　
+// > Custom orders
+// mutableListOf("one", "two", "three", "four").sortedBy { it.length }  = [one, two, four, three]
+// mutableListOf("one", "two", "three", "four").sortedByDescending { it.length }  = [three, four, one, two]
+// 　
+// > Custom orders:custom order for the collection sorting
+// mutableListOf("one", "two", "three", "four").sortedWith(compareBy { it.length })  = [one, two, four, three]
+// 　
+// > Reverse order
+// mutableListOf("one", "two", "three", "four").reversed()  = [four, three, two, one]
+// 　
+// > Reverse order:all its changes reflect in its reversed
+// reversedNumbers = mutableListOf("one", "two", "three", "four").asReversed() : [four, three, two, one]
+// numbers.add("five")
+// reversedNumbers = [five, four, three, two, one]
+// 　
+// > Random order
+// mutableListOf("one", "two", "three", "four").shuffled()  = [four, three, one, two]
