8bdabcaa5bcdc50921ff77c8bd057b04ded72dbc
kazenetu <rakudafactory@gmail.com>
2020-04-14 20:41:08 +0900

    SealedClassの実装例を追加

---
+++ OfficialSite/Class/SealedClass.kt
@@ -0,0 +1,30 @@
+// SealedClass「Expr」の派生確認用クラス定義
+sealed class Expr
+data class Const(val number: Double) : Expr()
+data class Sum(val e1: Expr, val e2: Expr) : Expr()
+object NotANumber : Expr()
+
+/*
+ * SealedClass
+ * 同一ファイル内のみで有効な抽象クラス
+ */
+fun main() {
+
+    // SealedClass「Expr」の派生確認
+    println("---SealedClass : when expression---")
+
+    // 派生毎の処理を行うメソッド
+    fun eval(expr: Expr): Double = when(expr) {
+        is Const -> expr.number
+        is Sum -> eval(expr.e1) + eval(expr.e2)
+        NotANumber -> Double.NaN
+    }
+
+    // 派生クラスの動作確認
+    println("eval(Const(1.5)) = ${ eval(Const(1.5)) }")
+    println("eval(Sum(Const(1.5), Const(0.5)) = ${ eval(Sum(Const(1.5), Const(0.5))) }")
+    println("eval(NotANumber) = ${ eval(NotANumber) }")
+
+}
+
+// 出力：
