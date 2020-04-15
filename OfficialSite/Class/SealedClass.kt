76767969018bb60cb553b579463f1d1c20064f10
kazenetu <rakudafactory@gmail.com>
2020-04-15 21:28:03 +0900

    SealedClassの具体的な実装例を追加

--- OfficialSite/Class/SealedClass.kt
+++ OfficialSite/Class/SealedClass.kt
@@ -4,6 +4,19 @@ data class Const(val number: Double) : Expr()
 data class Sum(val e1: Expr, val e2: Expr) : Expr()
 object NotANumber : Expr()
 
+// 実装例：色情報(参考:「Kotlinのsealed classを使いこなす」https://qiita.com/kikuchy/items/ad89a12029082be8d218)
+sealed class Color {
+    open class Rgb(val red: Int, val green: Int, val blue: Int): Color()
+    {
+        override fun toString(): String = "($red,$green,$blue)"
+    }
+
+    object Red:Rgb(255,0,0)
+    object Green:Rgb(0,255,0)
+    object Blue:Rgb(0,0,255)
+}
+
+
 /*
  * SealedClass
  * 同一ファイル内のみで有効な抽象クラス
@@ -24,6 +37,25 @@ fun main() {
     println("eval(Const(1.5)) = ${ eval(Const(1.5)) }")
     println("eval(Sum(Const(1.5), Const(0.5)) = ${ eval(Sum(Const(1.5), Const(0.5))) }")
     println("eval(NotANumber) = ${ eval(NotANumber) }")
+    println("　")
+
+    // 実装例：色情報(参考:「Kotlinのsealed classを使いこなす」https://qiita.com/kikuchy/items/ad89a12029082be8d218)
+    println("---SealedClass : Example Color---")
+
+    // 色単位で文字列を返す
+    fun GetText(src: Color): String = 
+        when(src) {
+            is Color.Red -> "Red:$src"
+            is Color.Green -> "Green:$src"
+            is Color.Blue -> "Blue:$src"
+            is Color.Rgb ->  "RGB:$src"
+        }
+
+    // 色情報付きの文字列を表示
+    println("GetText(Color.Rgb(1,2,3)) = ${ GetText(Color.Rgb(1,2,3)) }")
+    println("GetText(Color.Red) = ${ GetText(Color.Red) }")
+    println("GetText(Color.RGreened) = ${ GetText(Color.Green) }")
+    println("GetText(Color.Blue) = ${ GetText(Color.Blue) }")
 
 }
 
@@ -32,3 +64,9 @@ fun main() {
 // eval(Const(1.5)) = 1.5
 // eval(Sum(Const(1.5), Const(0.5)) = 2.0
 // eval(NotANumber) = NaN
+// 　
+// ---SealedClass : Example Color---
+// GetText(Color.Rgb(1,2,3)) = RGB:(1,2,3)
+// GetText(Color.Red) = Red:(255,0,0)
+// GetText(Color.RGreened) = Green:(0,255,0)
+// GetText(Color.Blue) = Blue:(0,0,255)
