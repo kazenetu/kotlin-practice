fefaa96c35bc0941b672f36e181cf25da7e36d41
kazenetu <rakudafactory@gmail.com>
2020-05-28 21:32:33 +0900

    コルーチンの通常/例外の最終処理の出力結果を追加

--- OfficialSite/Coroutine/AsynchronousFlow/FlowCompletion.kt
+++ OfficialSite/Coroutine/AsynchronousFlow/FlowCompletion.kt
@@ -68,3 +68,41 @@ fun main() = runBlocking<Unit> {
 }
 
 // 出力：
+// ---Flow completion---
+// > try-catch-finally
+// Emitting 1
+// 1
+// Emitting 2
+// 2
+// Emitting 3
+// 3
+// Done
+// 　
+// > Declarative handling
+// Emitting 1
+// 1
+// Emitting 2
+// 2
+// Emitting 3
+// 3
+// Done
+// 　
+// > Declarative handling:exception
+// 1
+// Flow completed exceptionally
+// Caught exception
+// 　
+// > Successful completion
+// Emitting 1
+// 1
+// Emitting 2
+// 2
+// Emitting 3
+// 3
+// 　
+// > Successful completion:exception
+// Emitting 1
+// 1
+// Emitting 2
+// Flow completed with java.lang.IllegalStateException: Collected 2
+// Exception in thread "main" java.lang.IllegalStateException: Collected 2
