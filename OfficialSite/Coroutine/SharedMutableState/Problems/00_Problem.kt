413842d3205cce71890bdb80115f4c8cee64111b
kazenetu <rakudafactory@gmail.com>
2020-07-12 22:06:36 +0900

    コルーチンの「共有された変更可能な状態と並行性：問題点1」の実装例を追加

---
+++ OfficialSite/Coroutine/SharedMutableState/Problems/00_Problem.kt
@@ -0,0 +1,47 @@
+import kotlinx.coroutines.*
+import kotlin.system.*
+
+suspend fun massiveRun(action: suspend () -> Unit) {
+    // コルーチン数
+    val n = 100
+
+    // 1コルーチンでの繰り返し回数
+    val k = 1000
+    val time = measureTimeMillis {
+        coroutineScope {
+            repeat(n) {
+                launch {
+                    repeat(k) { action() }
+                }
+            }
+        }
+    }
+    println("Completed ${n * k} actions in $time ms")  
+}
+
+var counter = 0
+
+/*
+ * コルーチン：共有された変更可能な状態と並行性：問題点1
+ * 変更可能な共有状態へのアクセスの同期が問題となる
+ */
+fun main() = runBlocking<Unit> {
+
+    // 改善前の問題点
+    // それぞれのコルーチンがcounterを同期せずに複数のスレッドから同時にインクリメントする。
+    // そのため「Counter = 100000」を出力することはほぼない。
+    println("---The problem---")
+
+    withContext(Dispatchers.Default) {
+        massiveRun {
+            // 単一のリソースをインクリメントして更新
+            counter++
+        }
+    }
+
+    // 最終の値を出力
+    println("Counter = $counter")
+
+}
+
+// 出力：
